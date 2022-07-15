86ff0ac07d97f4c683945512acbe790a6780b655
VladPulyak <vlad.pulyak03@mail.ru>
2022-07-16 02:29:39 +0300

    Added Homework 3 Task 1:Calculator

---
+++ TMS Homework 3/Task 1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                checked
+                {
+                    Console.WriteLine("Enter first number");
+                    double number1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter second number");
+                    double number2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter operation");
+                    string operation = Console.ReadLine();
+                    switch (operation)
+                    {
+                        case "+":
+                            {
+                                Console.WriteLine($"Result: { number1 + number2}");
+                                break;
+                            }
+                        case "-":
+                            {
+                                Console.WriteLine($"Result: { number1 - number2}");
+                                break;
+                            }
+                        case "*":
+                            {
+                                Console.WriteLine($"Result: { number1 * number2}");
+                                break;
+                            }
+                        case "/":
+                            {
+                                if (number2 == 0)
+                                {
+                                    throw new DivideByZeroException();
+                                }
+                                Console.WriteLine($"Result: { number1 / number2}");
+                                break;
+                            }
+                        case "%":
+                            {
+                                if (number2 == 0)
+                                {
+                                    throw new DivideByZeroException();
+                                }
+                                Console.WriteLine($"Result: { number1 % number2}");
+                                break;
+                            }
+                    }
+                }            
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("OverflowException, you entered wrong number");
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("DivideByZeroException, you entered number2 by zero");
+            }
+        }
+    }
+}
