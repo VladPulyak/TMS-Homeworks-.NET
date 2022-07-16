52c85a04432693ecb12123108626c0553ba315a3
VladPulyak <vlad.pulyak03@mail.ru>
2022-07-17 00:57:28 +0300

    Some changes in Task 1 and added Task 2

--- TMS Homework 3/Task 1/Program.cs
+++ TMS Homework 3/Task 1/Program.cs
@@ -6,71 +6,81 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
-            try
+            Console.WriteLine("To start calculator press any key expect Backspace");
+            ConsoleKeyInfo key = Console.ReadKey();
+            string keyString = key.Key.ToString();
+            while (keyString != "Backspace")
             {
-                checked
+                try
                 {
-                    Console.WriteLine("Enter first number");
-                    double number1 = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Enter second number");
-                    double number2 = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Enter operation");
-                    char operation = char.Parse(Console.ReadLine());
-                    switch (operation)
+                    checked
                     {
-                        case '+':
-                            {
-                                Console.WriteLine($"Result: { number1 + number2}");
-                                break;
-                            }
-                        case '-':
-                            {
-                                Console.WriteLine($"Result: { number1 - number2}");
-                                break;
-                            }
-                        case '*':
-                            {
-                                Console.WriteLine($"Result: { number1 * number2}");
-                                break;
-                            }
-                        case '/':
-                            {
-                                if (number2 == 0)
+                        Console.WriteLine("Enter first number");
+                        double number1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter second number");
+                        double number2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter operation");
+                        char operation = char.Parse(Console.ReadLine());
+                        switch (operation)
+                        {
+                            case '+':
                                 {
-                                    throw new DivideByZeroException();
+                                    Console.WriteLine($"Result: { number1 + number2}");
+                                    break;
                                 }
-                                Console.WriteLine($"Result: { number1 / number2}");
-                                break;
-                            }
-                        case '%':
-                            {
-                                if (number2 == 0)
+                            case '-':
                                 {
-                                    throw new DivideByZeroException();
+                                    Console.WriteLine($"Result: { number1 - number2}");
+                                    break;
                                 }
-                                Console.WriteLine($"Result: { number1 % number2}");
-                                break;
-                            }
-                        default:
-                            {
-                                Console.WriteLine("FormatException, you entered wrong number");
-                                break;
-                            }
+                            case '*':
+                                {
+                                    Console.WriteLine($"Result: { number1 * number2}");
+                                    break;
+                                }
+                            case '/':
+                                {
+                                    if (number2 == 0)
+                                    {
+                                        throw new DivideByZeroException();
+                                    }
+                                    Console.WriteLine($"Result: { number1 / number2}");
+                                    break;
+                                }
+                            case '%':
+                                {
+                                    if (number2 == 0)
+                                    {
+                                        throw new DivideByZeroException();
+                                    }
+                                    Console.WriteLine($"Result: { number1 % number2}");
+                                    break;
+                                }
+                            default:
+                                {
+                                    Console.WriteLine("FormatException, you entered wrong number");
+                                    break;
+                                }
+                        }
                     }
                 }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("OverflowException, you entered too large number");
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("DivideByZeroException, you entered number1 by zero");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("FormatException, you entered wrong number");
+                }
+                Console.WriteLine(@"If you would like to exit - press Backspace, continue - press any key ");
+                key = Console.ReadKey();
+                keyString = key.Key.ToString();
             }
-            catch (OverflowException)
-            {
-                Console.WriteLine("OverflowException, you entered too large number");
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("DivideByZeroException, you entered number1 by zero");
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("FormatException, you entered wrong number");
-            }
+            Console.WriteLine("You exit calculator");
         }
     }
 }
---
+++ TMS Homework 3/Task 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            int numA = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int numB = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third number: ");
+            int numC = int.Parse(Console.ReadLine());
+            if (numA > 100 && numB > 100)
+            {
+                Console.WriteLine("Каждое из чисел А и В больше 100");
+            }
+            if ((numA % 2 == 0 && numB % 2 != 0) || (numA % 2 != 0 && numB % 2 == 0)) //(numA % 2 == 0 ^ numB % 2 == 0)
+            {
+                Console.WriteLine("Только одно из чисел А и В четное");
+            }
+            if (numA > 0 || numB > 0)
+            {
+                Console.WriteLine("Одно из чисел А и В положительное");
+            }
+            if (numA % 3 == 0 && numB % 3 == 0 && numC % 3 == 0)
+            {
+                Console.WriteLine("Каждое из чисел А, В, С кратно трем");
+            }
+            if ((numA < 50 && numB > 50 && numC > 50) || (numA > 50 && numB < 50 && numC > 50) || (numA > 50 && numB > 50 && numC < 50))
+            {
+                Console.WriteLine("Только одно из чисел А, В и С меньше 50");
+            }
+            if (numA < 0 || numB < 0 || numC < 0)
+            {
+                Console.WriteLine("Одно из чисел А, В, С отрицательное");
+            }
+        }
+    }
+}
