041248cdfea6d02c6b9101b1f5349f6e94e9533f
VladPulyak <vlad.pulyak03@mail.ru>
2022-07-28 13:50:35 +0300

    Added Homework 6(Classes)

---
+++ Homework 6/Task 1/Phone.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Phone
+    {
+        double _weight;
+
+        public Phone(string number, string model, double weight)
+        {
+            Number = number;
+            Model = model;
+            Weight = weight;
+        }
+
+        public string Number { get; set; }
+
+        public string Model { get; set; }
+
+        public double Weight
+        {
+            get
+            {
+                return _weight;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _weight = 0;
+                    return;
+                }
+                _weight = value;
+            }
+        }
+
+        public void RecieveCall(string name)
+        {
+            Console.WriteLine($"{name} is calling you");
+        }
+    }
+}
---
+++ Homework 6/Task 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone iPhone = new Phone("+375298961247", "IPhone XS Max", 208);
+            Phone samsung = new Phone("+375257891235", "Samsung S8", 168);
+            Console.WriteLine(samsung.Model);
+            Console.WriteLine(iPhone.Number);
+            Console.WriteLine(samsung.Weight);
+            samsung.Weight = -56;
+            Console.WriteLine(samsung.Weight);
+            samsung.RecieveCall("Vlad");
+            iPhone.RecieveCall("Kolya");
+
+        }
+    }
+}
