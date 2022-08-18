e99587be24cd87f8efd908f9432e646c086289d4
VladPulyak <vlad.pulyak03@mail.ru>
2022-08-18 16:43:11 +0300

    Added Homework 10

---
+++ Homework 10/Microwave Event/Microwave.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microwave_Event
+{
+    public delegate void NotifyWarmUpCompleted(string foodName);
+    internal class Microwave
+    {
+        public event NotifyWarmUpCompleted WarmUpCompleted;
+        public void WarmUp(string foodName)
+        {
+            Console.WriteLine($"I warm up {foodName}");
+            WarmUpCreated(foodName);
+        }
+        protected virtual void WarmUpCreated(string foodName)
+        {
+            if(WarmUpCompleted != null)
+            {
+                WarmUpCompleted.Invoke(foodName);
+            }
+        }
+    }
+}
---
+++ Homework 10/Microwave Event/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Microwave_Event
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Microwave microwave = new Microwave();
+            microwave.WarmUpCompleted += WarmUpCompletedHandler1;
+            microwave.WarmUp("Cheese");
+            microwave.WarmUpCompleted += WarmUpCompletedHandler2;
+            microwave.WarmUp("Pizza");
+        }
+        private static void WarmUpCompletedHandler1(string foodName)
+        {
+            Console.WriteLine($"{foodName} is ready!");
+        }
+        private static void WarmUpCompletedHandler2(string foodName)
+        {
+            Console.WriteLine($"{foodName} is ready!");
+        }
+    }
+}
