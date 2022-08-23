1efe6a5886d76ac8915ad0196139ecd2bee56a9b
VladPulyak <vlad.pulyak03@mail.ru>
2022-08-23 14:07:46 +0300

    Added Homework 11

---
+++ Homework 11/Task 1/Employee.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Employee
+    {
+        private int _age;
+        private string _name;
+        private double _salary;
+
+        public Employee(int age, string name, double salary)
+        {
+            Age = age;
+            Name = name;
+            Salary = salary;
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value < 17)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    _age = value;
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == null || value == string.Empty)
+                {
+                    throw new ArgumentNullException();
+                }
+                if (value.Length > 100)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public double Salary
+        {
+            get { return _salary; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    _salary = value;
+                }
+            }
+        }
+    }
+}
---
+++ Homework 11/Task 1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1;
+            Console.WriteLine("Enter infoemation about employee: Age, Name, Salary");
+            int age = int.Parse(Console.ReadLine());
+            string name = Console.ReadLine();
+            double salary = double.Parse(Console.ReadLine());
+            try
+			{
+                employee1 = new Employee(age, name, salary);
+			}
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.TargetSite);
+                Console.WriteLine($"Stack Trace:\n {ex.StackTrace}");
+            }
+            catch (ArgumentException ex)
+			{
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.TargetSite);
+                Console.WriteLine($"Stack Trace:\n {ex.StackTrace}");
+			}
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+            Console.WriteLine("Press Enter to exit program");
+            Console.ReadKey();
+        }
+    }
+}
