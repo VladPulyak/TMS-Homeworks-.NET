ad179ae907f5325ed2ea82786879849b6b9325a5
VladPulyak <vlad.pulyak03@mail.ru>
2022-09-01 21:58:48 +0300

    Added Homework 13

---
+++ Homework 13/Homework 13/Controllers/BookController.cs
@@ -0,0 +1,14 @@
+﻿using Homework_13.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_13.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            BookViewModel book = new BookViewModel("Mumu", "Tyrgenev", 600);
+            return View(book);
+        }
+    }
+}
---
+++ Homework 13/Homework 13/Models/BookViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework_13.Models
+{
+    public class BookViewModel
+    {
+        public BookViewModel(string bookName, string author, int countOfPages)
+        {
+            BookName = bookName;
+            Author = author;
+            CountOfPages = countOfPages;
+        }
+
+        public string BookName  { get; set; }
+        public string Author { get; set; }
+        public int CountOfPages { get; set; }
+    }
+}
