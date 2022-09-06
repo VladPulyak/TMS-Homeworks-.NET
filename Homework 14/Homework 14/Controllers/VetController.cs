a578b5139edc7ec890d7b2e8ff01aac58365a8d1
VladPulyak <vlad.pulyak03@mail.ru>
2022-09-06 21:15:23 +0300

    Added Homework 14(Dogs)

---
+++ Homework 14/Homework 14/Controllers/VetController.cs
@@ -0,0 +1,28 @@
+﻿using Homework_14.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_14.Controllers
+{
+    public class VetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult VetApplication()
+        {
+            ModelState.Clear();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult VetApplication(DogViewModel dog)
+        {
+            Console.WriteLine($"Name of your dog:{dog.PetName}");
+            Console.WriteLine($"Master name:{dog.HostName}");
+            Console.WriteLine($"Age of your dog:{dog.Age}");
+            Console.WriteLine($"Weight of your dog:{dog.Weight}");
+            return VetApplication();
+        }
+    }
+}
---
+++ Homework 14/Homework 14/Models/DogViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_14.Models
+{
+    public class DogViewModel
+    {
+        public string? PetName { get; set; }
+        public string? HostName { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+    }
+}
